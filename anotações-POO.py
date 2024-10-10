aa6599b115f99ec70abf55a9f8d58be6d0644056
eduarda <143198574+eduardamedeirosramos@users.noreply.github.com>
2024-10-10 14:31:36 -0300

    Add files via upload

---
+++ anotações-POO.py
@@ -0,0 +1,12 @@
+#POO- básico:conceito do mundo real atráves de códigos.
+#usando a classe gera objetos
+
+# na prática e teoria:classe=
+                      #atributos= caracteristicas/váriaveis
+                      #métodos= ações/função
+
+#pilares:
+         #abstração = simplificar
+         #encapsulamento = proteger
+         #herança = capacidade de herdar de outras classes(existe entre classes)
+         #polimorfismo = várias formas(adaptar o método herdado)
